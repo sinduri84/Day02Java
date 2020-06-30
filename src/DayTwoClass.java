085ce8723ad02c03c3531b460e05a354dc67891c
Sinduri Guntupalli <sinduri.g@gmail.com>
2020-06-30 10:36:58 +0200

    A1 Exercise

---
+++ src/DayTwoClass.java
@@ -0,0 +1,18 @@
+public class DayTwoClass {
+    public static void main(String[] args) {
+        String hannah = "Did Hannah see bees? Hannah did";
+        System.out.println(hannah);
+
+        //A1.1
+        System.out.println("");
+        System.out.println("A1.1");
+
+        System.out.println("The length of the string hannah is " + hannah.length() + ".");
+
+        //A1.2
+        System.out.println("");
+        System.out.println("A1.2");
+
+
+    }
+}
